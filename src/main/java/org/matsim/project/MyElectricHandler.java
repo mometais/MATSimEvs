632a12d4d75d5345269f0db5b35137305d0bf037
Sebastian Hörl <hoerl.sebastian@gmail.com>
2021-04-02 14:46:18 +0200

    test electric handler

---
+++ src/main/java/org/matsim/project/MyElectricHandler.java
@@ -0,0 +1,28 @@
+package org.matsim.project;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.events.VehicleLeavesTrafficEvent;
+import org.matsim.api.core.v01.events.handler.VehicleLeavesTrafficEventHandler;
+import org.matsim.contrib.ev.fleet.ElectricFleet;
+import org.matsim.contrib.ev.fleet.ElectricVehicle;
+import org.matsim.core.events.MobsimScopeEventHandler;
+import org.matsim.vehicles.Vehicle;
+
+import com.google.inject.Inject;
+
+public class MyElectricHandler implements VehicleLeavesTrafficEventHandler, MobsimScopeEventHandler {
+	@Inject
+	public ElectricFleet electricFleet;
+
+	@Override
+	public void handleEvent(VehicleLeavesTrafficEvent event) {
+		Id<Vehicle> vehicleId = event.getVehicleId();
+		Id<ElectricVehicle> evId = Id.create(vehicleId, ElectricVehicle.class);
+
+		ElectricVehicle ev = electricFleet.getElectricVehicles().get(evId);
+		
+		if (ev != null) {
+			System.err.println("EV charging state: " + ev.getBattery().getSoc());
+		}
+	}
+}
--- src/main/java/org/matsim/project/RunWithEvs.java
+++ src/main/java/org/matsim/project/RunWithEvs.java
@@ -19,19 +19,12 @@
 package org.matsim.project;
 
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.api.core.v01.TransportMode;
 import org.matsim.contrib.ev.EvConfigGroup;
 import org.matsim.contrib.ev.EvModule;
-import org.matsim.contrib.ev.MobsimScopeEventHandler;
-import org.matsim.contrib.ev.charging.VehicleChargingHandler;
-import org.matsim.contrib.ev.fleet.ElectricVehicle;
-import org.matsim.contrib.ev.routing.EvNetworkRoutingProvider;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.Injector;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.mobsim.qsim.AbstractQSimModule;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -55,32 +48,20 @@ public class RunWithEvs {
 		evConfigGroup.setVehiclesFile("../../scenarios/equil/evs.xml");
 		config.addModule(evConfigGroup);
 
-		
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
 
 		Controler controler = new Controler( scenario ) ;
 
 		EvModule evModule = new EvModule();
 		controler.addOverridingModule(evModule);
-
-		AbstractModule abstractModule = new AbstractModule() {
-			@Override
-			public void install() {
-				addRoutingModuleBinding(TransportMode.car).toProvider(new EvNetworkRoutingProvider(TransportMode.car));
-				installQSimModule(new AbstractQSimModule() {
-					@Override
-					protected void configureQSim() {
-						bind(VehicleChargingHandler.class).asEagerSingleton();
-						bind(MobsimScopeEventHandler.class).to(VehicleChargingHandler.class);
-					}
-				});
-			}
-		};
-		controler.addOverridingModule(abstractModule);
 		controler.configureQSimComponents(components -> components.addNamedComponent(EvModule.EV_COMPONENT));
-
 		
-
+		controler.addOverridingQSimModule(new AbstractQSimModule() {
+			@Override
+			protected void configureQSim() {
+				addMobsimScopeEventHandlerBinding().to(MyElectricHandler.class);
+			}
+		});
 		
 		controler.run();
 	}
