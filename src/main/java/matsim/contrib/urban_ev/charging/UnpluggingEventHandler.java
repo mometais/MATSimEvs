08b4dec58cac20c20179eb636a3e71d77638277b
marcoliviermetais <marcolivier.metais@gmail.com>
2021-08-10 11:29:07 +0200

    delete matsim urban_ev, dependencies problems. Moving to another package, this project stays at this state for the moment

--- src/main/java/matsim/contrib/urban_ev/charging/UnpluggingEventHandler.java
+++
@@ -1,8 +0,0 @@
-package de.tum.mw.ftm.matsim.contrib.urban_ev.charging;
-
-import org.matsim.core.events.handler.EventHandler;
-
-public interface UnpluggingEventHandler extends EventHandler {
-    void handleEvent(UnpluggingEvent event);
-
-}
--- src/main/java/matsim/contrib/urban_ev/scoring/ChargingBehaviourScoringEventHandler.java
+++
@@ -1,8 +0,0 @@
-package de.tum.mw.ftm.matsim.contrib.urban_ev.scoring;
-
-import org.matsim.core.events.handler.EventHandler;
-
-public interface ChargingBehaviourScoringEventHandler extends EventHandler {
-    void handleEvent(ChargingBehaviourScoringEvent event);
-
-}
